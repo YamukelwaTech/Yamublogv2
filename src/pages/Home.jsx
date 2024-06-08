aac84a577c30c9fdb8b75ace63bf3d43930d36f9
YamuTech <157011521+YamukelwaTech@users.noreply.github.com>
2024-06-08 23:19:13 +0200

    Chore:Footer added

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import HeroSection from "../components/hero2";
 import Services from "../components/services";
+import Footer from "../components/footer";
 
 const Home = () => {
   return (
     <>
       <HeroSection />
       <Services />
+      <Footer />
     </>
   );
 };
