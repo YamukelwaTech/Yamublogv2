840f74e8c035e556da9b98a21dd02dc37c6e83d1
YamuTech <157011521+YamukelwaTech@users.noreply.github.com>
2024-06-08 22:54:19 +0200

    Chore:feats added

--- src/components/hero2.jsx
+++ src/components/hero2.jsx
@@ -6,9 +6,6 @@ const HeroSection = () => {
       <div className="px-4 mx-auto max-w-7xl sm:px-6 lg:px-8">
         <div className="grid items-center grid-cols-1 gap-12 lg:grid-cols-2">
           <div>
-            <p className="text-base font-bold tracking-wider text-customColor2 uppercase">
-              The Blogging Site You need
-            </p>
             <h1 className="mt-4 text-4xl font-bold text-black lg:mt-8 sm:text-6xl xl:text-8xl">
               Track, share, measure—you have full control.
             </h1>
@@ -19,7 +16,7 @@ const HeroSection = () => {
             <a
               href="1"
               title=""
-              className="inline-flex items-center px-6 py-4 mt-8 font-semibold text-black transition-all duration-200 bg-yellow-300 rounded-full lg:mt-16 hover:bg-yellow-400 focus:bg-yellow-400"
+              className="inline-flex items-center px-6 py-4 mt-8 font-semibold text-black transition-all duration-200 bg-customColor3 rounded-full lg:mt-16 hover:bg-yellow-600 focus:bg-ycustomColor3"
               role="button"
             >
               Join for free
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import HeroSection from "../components/hero2";
+import Services from "../components/services";
 
 const Home = () => {
   return (
     <>
       <HeroSection />
+      <Services />
     </>
   );
 };
